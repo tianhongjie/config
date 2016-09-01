a5c94ed9e7ab6c00852cb253c22417a1cdc44a90
Charlie Tian <charlie.tian@spsoft-cn.com>
2016-09-01 09:08:01 +0800

    [ADD] add install bash

---
+++ ubuntu.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+sudo apt-get update
+
+sudo apt-get install vim -y
+sudo apt-get install git -y
+sudo apt-get install gitg -y
+sudo apt-get install nginx -y
+sudo apt-get install supervisor -y
+
+sudo apt-get install postgresql-server-dev-9.5 postgresql-9.5 postgresql-client-9.5 postgresql-contrib-9.5 postgresql-client-common pgadmin3 -y
+
+sudo apt-get install python-pip python-dev build-essential -y
+sudo pip install virtualenv virtualenvwrapper
+sudo pip install --upgrade pip
+
+sudo apt-get install wiznote -y
+sudo apt-get install autojump zsh tree ipython git-flow elasticsearch -y
+sudo apt-get install redis-server -y
+sudo apt-get install unity-tweak-tool -y
+sudo apt-get install ctags -y
+
+sudo apt-get install cmake -y
+sudo apt-get install libgtk2.0-dev -y
+
+sudo apt-get install -y python-dev python-virtualenv
+sudo apt-get install -y gunicorn supervisor nginx git rsync
+sudo apt-get install gcc -y
+sudo apt-get install python-pip -y
+sudo pip install virtualenv
+
