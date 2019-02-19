3f8084db84051269d101c40ec025beddb65b0c3c
Charlie Tian <tianhongjie07@gmail.com>
2019-02-19 13:14:18 +0800

    Add files via upload

---
+++ install_ubuntu_base.sh
@@ -0,0 +1,12 @@
+sudo apt udpate
+sudo apt upgrade -y
+sudo apt remove -y --purge ibus*
+sudo apt install -y fcitx fcitx-googlepinyin
+
+sudo apt install -y chromium-browser
+sudo apt install -y openjdk-8-jdk
+
+sudo apt install -y vim git gitg git-flow 
+sudo apt install -y python-pip python-dev
+sudo apt install -y autojump zsh tree ipython cmake libgtk2.0-dev
+sudo apt install -y nginx supervisor
