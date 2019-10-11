c6cc5ed8b32c535341b7016ec8e389bd481d75ff
Charlie Tian <tianhongjie07@gmail.com>
2019-10-11 15:27:47 +0800

    Add files via upload

---
+++ install_ubuntu.sh
@@ -0,0 +1,8 @@
+sudo apt install -y openssh-server vim \
+	gimp \
+	fcitx-googlepinyin \
+	p7zip-full \
+	 autojump zsh tree ipython \
+	 vim git gitg git-flow nginx supervisor \
+	 chromium-browser \
+	 chrome-gnome-shell 
